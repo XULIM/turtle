0d5da6580e535c95bf09742260ae27a615647bb5
XULIM <67230458+XULIM@users.noreply.github.com>
2022-04-15 00:16:56 -0400

    minor change (backspace)

--- fib.py
+++ fib.py
@@ -2,8 +2,7 @@ from functools import lru_cache
 import turtle
 @lru_cache(maxsize=None)
 def fib(n):
-    if n < 2:
-        return n
+    if n < 2: return n
     return fib(n-1) + fib(n-2)
 def plot(): 
     for i in range(4):
